a72b2ae7f7b6118349179f55bff1fd29dd00798b
Mythic Frost <77963571+Mythic-Frost@users.noreply.github.com>
2024-01-14 11:22:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570, 346110, 466240, 1172470];  // Enter here AppIDs of the needed games
+var games = [730, 570, 346110, 466240, 1172470];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
